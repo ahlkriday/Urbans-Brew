19311581cf3574975fbf1d3203c2dde64534c9d9
ahlkriday <ahl.kriday@gmail.com>
2026-02-08 20:55:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const scrollReveal = () => {
     const reveals = document.querySelectorAll('.reveal');
-    reveals.forEach((element, index) => {
+    reveals.forEach((element) => {
         const windowHeight = window.innerHeight;
         const elementTop = element.getBoundingClientRect().top;
-        const elementVisible = 150;
-        
-        if (elementTop < windowHeight - elementVisible) {
-            setTimeout(() => {
-                element.classList.add('active');
-            }, index * 50); // Clean staggered entry
+        if (elementTop < windowHeight - 150) {
+            element.classList.add('active');
         }
     });
 }
